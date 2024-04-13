42cf7a34fb48b0768002208841ceaa78cbeec07a
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-13 20:32:33 +0000

    Implemento la funcion cargar_memoria_config para que a partir del struct t_config se cargue sus datos en el struct t_memoria_config

--- memoria/src/memoria_config.c
+++ memoria/src/memoria_config.c
@@ -13,3 +13,13 @@ t_memoria_config* crear_memoria_config(){
 void destruir(t_memoria_config* mc){
 
 }
+
+bool cargar_memoria_config(t_memoria_config* mc, t_config* cf){
+    mc->PUERTO_ESCUCHA = config_get_int_value(cf, "PUERTO_ESCUCHA");
+    mc->TAM_MEMORIA = config_get_int_value(cf, "TAM_MEMORIA");
+    mc->TAM_PAGINA = config_get_int_value(cf, "TAM_PAGINA");
+    mc->PATH_INSTRUCCIONES = config_get_string_value(cf, "PATH_INSTRUCCIONES");
+    mc->RETARDO_RESPUESTA = config_get_int_value(cf, "RETARDO_RESPUESTA");
+
+    return true;
+}
