45063cd720762d6b66d0e021d9a0a21177befa39
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-13 20:28:39 +0000

    Creo archivo principal 'memoria.h' del modulo memoria

---
+++ memoria/include/src/memoria.h
@@ -0,0 +1,14 @@
+#ifndef MEMORIA_H_
+#define MEMORIA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include "sockets.h"
+#include "inicializar_memoria.h"
+
+#define SERVERNAME "MEMORIA"
+
+#endif /*MEMORIA_H*/
