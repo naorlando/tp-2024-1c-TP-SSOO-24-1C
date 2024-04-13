f4cb7d0dd44a480c5ded2586d9220b3bdb75ff69
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-13 20:35:20 +0000

    Implemento la funcion '_iniciar_logger' que se encargara se crear los loggers que usara el modulo de memoria

--- memoria/src/inicializar_memoria.c
+++ memoria/src/inicializar_memoria.c
@@ -4,3 +4,19 @@ t_log* logger_memoria;
 t_log* logger_memoria_debug;
 t_config* config_memoria;
 t_memoria_config* memoria_config;
+
+void _iniciar_logger(){
+    logger_memoria = log_create("memoria.log", "MEMORIA_LOG", 1, LOG_LEVEL_INFO);
+
+    if(logger_memoria == NULL){
+        perror("No se pudo crear el archivo log para el modulo de memoria");
+        //exit(EXIT_FAILURE);
+    }
+
+    logger_memoria_debug = log_create("memoria.log", "MEMORIA_LOG", 1, LOG_LEVEL_TRACE);
+
+    if(logger_memoria_debug == NULL){
+        perror("No se pudo crear el archivo log de debug para el modulo de memoria");
+        //exit(EXIT_FAILURE);
+    }
+}
