71646a1ed866063498f2ec05f04d5e735c0a00b3
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-13 20:42:08 +0000

    Creo funcion main y levanto el socket de escucha del modulo memoria

---
+++ memoria/src/memoria.c
@@ -0,0 +1,20 @@
+#include "memoria.h"
+
+int main(void){
+    init();
+
+    // ############## Socket ###################
+
+    char* server_port = string_itoa(memoria_config->PUERTO_ESCUCHA);
+    int fd_server = iniciar_servidor(logger_memoria, NULL, NULL, server_port);
+
+    if (fd_server != -1) {
+        log_info(logger_memoria, "%s server listo escuchando en puerto %s", SERVERNAME, server_port);
+    } else {
+        log_error(logger_memoria, "Error al iniciar %s server en puerto %s", SERVERNAME, server_port);
+    }
+
+    free(server_port);
+
+    return EXIT_SUCCESS;
+}
