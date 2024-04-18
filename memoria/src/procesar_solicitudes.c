3ae3ae8cf5bfef1b0865eee2d2c0acc7157d2bd8
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-18 03:34:46 +0000

    Agrego implementacion de la funcion 'solicitudes_entradasalida'

--- memoria/src/procesar_solicitudes.c
+++ memoria/src/procesar_solicitudes.c
@@ -51,3 +51,29 @@ void solicitudes_kernel(int fd_kernel, t_log* logger) {
         }
     }
 }
+
+void solicitudes_entradasalida(int fd_entradasalida, t_log* logger) {
+    bool esperar = true;
+
+    while(esperar) {
+        int cod_operacion = recibir_operacion(fd_entradasalida);
+
+        switch(cod_operacion){
+            case MENSAJE:
+                // Se procesa el request
+                recibir_mensaje(logger, fd_cpu);
+            break;
+            //TODO:
+            /*
+                Agregar operaciones a las que dara servicio el modulo
+            */ 
+            case ERROR:
+                log_error(logger, "ERROR: Ha surgido un problema inesperado, se desconecto el modulo de memoria.");
+                esperar = false; //Cortamos la espera de solicitudes
+            break;
+            default:
+                log_warning(logger, "WARNING: El modulo de memoria ha recibido una solicitud con una operacion desconocida");
+            break;
+        }
+    }
+}
