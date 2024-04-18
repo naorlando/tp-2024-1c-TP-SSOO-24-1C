8b5cd746ceed747d32ed6b0e2d175af73d1225fe
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-18 03:44:50 +0000

    Agrego firma de la funcion 'recibr_buffer'

--- utils/src/utils/sockets.h
+++ utils/src/utils/sockets.h
@@ -17,6 +17,7 @@ int esperar_cliente(t_log* logger, const char* name, int socket_servidor);
 int crear_conexion(t_log* logger, const char* server_name, char* ip, char* puerto);
 void liberar_conexion(int socket_cliente);
 void recibir_mensaje(t_log*, int);
+void* recibir_buffer(int*, int);
 
 
 #endif
