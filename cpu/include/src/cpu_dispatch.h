e6f65f29f8ab6b9f0f1801da701cdd50ae44233d
LlopizJuan <llopizjuan@frba.utn.edu.ar>
2024-04-20 17:29:53 +0000

    se borro info cpu dispatch

--- cpu/include/src/cpu_dispatch.h
+++ cpu/include/src/cpu_dispatch.h
@@ -1,19 +1,6 @@
 #ifndef CPU_DISPATCH_H_
 #define CPU_DISPATCH_H_
 
-#include <commons/log.h>
-#include <commons/string.h>
-#include "utils/buffer.h"
-#include "utils/protocolo.h"
-#include "cpu.h"
-
-extern int fd_server_dispatch;
-extern int fd_kernel_dispatch;
-
-
-void atender_cpu_kernel_dispatch();
-
-
 
 
 #endif
