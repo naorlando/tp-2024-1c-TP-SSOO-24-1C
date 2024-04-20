1d84c16079eb70821253af351ff81a6c4d45f228
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-04-20 16:04:41 +0000

    Agrego tracking al  archivo 'protocolo_memoria.h'

---
+++ memoria/include/src/protocolo_memoria.h
@@ -0,0 +1,15 @@
+#ifndef PROTOCOLO_MEMORIA_H_
+#define PROTOCOLO_MEMORIA_H_
+
+#include <commons/log.h>
+#include <utils/sockets.h>
+#include "variables_globales.h"
+#include "utils/protocolo.h"
+
+void solicitudes_cpu();
+
+void solicitudes_kernel();
+
+void solicitudes_entradasalida();
+
+#endif /*PROTOCOLO_MEMORIA_H_*/
