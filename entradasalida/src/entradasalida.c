4a908a5f138b211d36f082ef00e01ef49c14fe4a
JorgeTrip <jtripodi@frba.utn.edu.ar>
2024-05-04 13:41:09 +0000

    Comento bloques de conexión con kernel y memoria y creo nuevo bloque de conexión con kernel.

--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -5,11 +5,19 @@ int main(int argc, char *argv[])
 
     init();
 
-    fd_memoria = crear_conexion(logger_entradasalida, SERVER_MEMORIA, IP_MEMORIA, PUERTO_MEMORIA);
-    fd_memoria > 0 ? send_example_memoria() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_MEMORIA);
+    // Comento bloque de conexión con memoria (checkpoint 1)
+    // fd_memoria = crear_conexion(logger_entradasalida, SERVER_MEMORIA, IP_MEMORIA, PUERTO_MEMORIA);
+    // fd_memoria > 0 ? send_example_memoria() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_MEMORIA);
 
+    // Comento bloque de conexión con kernel (checkpoint 1)
+    // fd_kernel = crear_conexion(logger_entradasalida, SERVER_KERNEL, IP_KERNEL, PUERTO_KERNEL);
+    // fd_kernel > 0 ? send_example_kernel() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_KERNEL);
+
+    // Reemplazo el código anterior quitando la conexión con Memoria, ya que la Interfaz Genérica no la necesita
+    
     fd_kernel = crear_conexion(logger_entradasalida, SERVER_KERNEL, IP_KERNEL, PUERTO_KERNEL);
-    fd_kernel > 0 ? send_example_kernel() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_KERNEL);
+    fd_kernel > 0 ? log_info(logger_entradasalida, "Conexión exitosa con kernel")
+                  : log_error(logger_entradasalida, "Error al conectar con kernel");
 
     // Atendemos mensaje del Kernel
     pthread_t hilo_kernel;
