bc3da5649e2803e9b39f46c13107dccd7e91690f
martin-vainer <vainermartin@gmail.com>
2024-05-06 14:19:38 +0000

    comento cosas

--- kernel/include/src/consola.h
+++ kernel/include/src/consola.h
@@ -8,6 +8,7 @@ void inicial_consola_interactiva();
 void _validacion_de_instrucciones_consola();
 void _atender_instruccion();
 void _iniciar_proceso();
-void _asignar_pid();
 
-#endif
+//void _asignar_pid();
+
+#endif //CONSOLA_H_
