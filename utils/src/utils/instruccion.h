f18c951ccc1bae7661853f300f8e1d87c472086b
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-05-24 13:43:16 +0000

    Reemplazo el struct t_instruccion por t_instruction, tambien cambie el nombre del struct en las diferentes partes donde se utilizaba

--- utils/src/utils/instruccion.h
+++ utils/src/utils/instruccion.h
@@ -6,23 +6,46 @@
 #include <string.h>
 #include <commons/string.h>
 
+typedef enum {
+    SET,
+    SUM,
+    SUB,
+    MOV_IN,
+    MOV_OUT,
+    RESIZE,
+    WAIT ,
+    SIGNAL ,
+    JNZ,
+    COPY_STRING,
+    IO_GEN_SLEEP ,
+    IO_STDIN_READ ,
+    IO_STDOUT_WRITE ,
+    IO_FS_CREATE ,
+    IO_FS_DELETE ,
+    IO_FS_TRUNCATE ,
+    IO_FS_WRITE ,
+    IO_FS_READ ,
+    EXIT
+}t_name_instruction;
+
 typedef struct {
-    char* linea_instruccion;
-} t_instruccion;
+    t_name_instruction name,
+    t_list * params
+}t_instruction;
 
 // Crea una nueva instrucción a partir de una línea de texto.
 // Pre: La línea de texto debe ser válida y no NULL.
 // Post: Retorna un puntero a una estructura t_instruccion con la línea de instrucción duplicada.
-t_instruccion* crear_instruccion(char*);
+t_instruction* crear_instruccion(char*);
 
 // Elimina una instrucción.
 // Pre: El puntero a t_instruccion debe ser válido y no NULL.
 // Post: La memoria asignada para la instrucción y su línea es liberada.
-void eliminar_instruccion(t_instruccion*);
+void eliminar_instruccion(t_instruction*);
 
 // Obtiene la línea de la instrucción.
 // Pre: El puntero a t_instruccion debe ser válido y no NULL.
 // Post: Retorna un puntero a la cadena de texto de la instrucción.
-char* obtener_instruccion(t_instruccion*);
+char* obtener_instruccion(t_instruction*);
 
 #endif
