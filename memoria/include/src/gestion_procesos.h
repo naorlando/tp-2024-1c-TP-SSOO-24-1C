a9de1dcf98910ab5327fae54382015dfd3803837
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-01 22:02:22 +0000

    Agrego descripciones a las funciones de 'gestion_procesos'

--- memoria/include/src/gestion_procesos.h
+++ memoria/include/src/gestion_procesos.h
@@ -5,8 +5,16 @@
 #include "proceso.h"
 #include "utils/estructuras.h"
 
+
+// Agrega un nuevo proceso a la memoria.
+// Pre: El puntero a t_new_process debe ser válido y no NULL.
+// Post: Retorna true si el proceso se ha agregado exitosamente a la memoria, 
+//       o false si hubo un error.
 bool add_process_to_memory(t_new_process*);
 
+// Obtiene la ruta absoluta dado un path relativo.
+// Pre: El puntero a path_relative debe ser válido y no NULL.
+// Post: Retorna un puntero a una cadena que contiene la ruta absoluta.
 char* _get_path_absolut(char*);
 
 #endif
