24ae7bcf9fb5d3f5f45da85c8888caf7372548a8
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-03 22:10:59 +0000

    Implemento las funciones 'levantar_servidor', 'inicializar_kernel', 'crear_hilos_conexiones' y 'cerrar_servidor' para que se encarguen de la configuracion del servidor kernel

--- kernel/include/src/kernel_server.h
+++ kernel/include/src/kernel_server.h
@@ -3,12 +3,22 @@
 
 #include "variables_globales.h"
 #include "protocolo_kernel.h"
+#include "commons/string.h"
+#include "utils/sockets.h"
 #include "utils/estructuras.h"
 #include "utils/buffer.h"
 #include "utils/sockets.h"
+#include <commons/log.h>
+#include "kernel_config.h"
+#include <pthread.h>
+
 
 void atender_kernel_memoria();
 void atender_kernel_IO();
 void atender_kernel_cpu_dispatch();
+void levantar_servidor();
+void inicializar_sockets();
+void crear_hilos_conexiones();
+void cerrar_servidor();
 
 #endif //KERNEL_SERVER
--- kernel/src/kernel_server.c
+++ kernel/src/kernel_server.c
@@ -85,3 +85,63 @@ void atender_kernel_cpu_dispatch()
         }
     }
 }
+
+void levantar_servidor()
+{
+    server_port = string_itoa(kernel_config->PUERTO_ESCUCHA);
+    fd_server = iniciar_servidor(logger_kernel, NULL, NULL, server_port);
+
+    if (fd_server != -1)
+    {
+        log_info(logger_kernel, "%s server listo escuchando en puerto %s", SERVERNAME, server_port);
+    }
+    else
+    {
+        log_error(logger_kernel, "Error al iniciar %s server en puerto %s", SERVERNAME, server_port);
+    }
+}
+
+void inicializar_sockets()
+{
+    // Conexion con cpu_dispatch
+    cpu_dispatch_port = string_itoa(kernel_config->PUERTO_CPU_DISPATCH);
+    fd_cpu_dispatch = crear_conexion(logger_kernel, SERVER_CPU, kernel_config->IP_CPU, cpu_dispatch_port);
+
+    // Conexion con memoria
+    memoria_port = string_itoa(kernel_config->PUERTO_MEMORIA);
+    fd_kernel_memoria = crear_conexion(logger_kernel, SERVER_MEMORIA, kernel_config->IP_MEMORIA, memoria_port);
+
+    //fd_kernel_memoria > 0 ? send_example_memoria() : log_error(logger_kernel, "Error al intentar enviar mensaje a %s", SERVER_MEMORIA);
+
+    log_info(logger_kernel, "Esperando a que se conecte %s...", CLIENTE_ENTRADASALIDA);
+    fd_kernel_IO = esperar_cliente(logger_kernel, CLIENTE_ENTRADASALIDA, fd_server);
+}
+
+void crear_hilos_conexiones() 
+{
+    // Hilo para manejar mensajes de CPU Dispatch
+    pthread_t hilo_cpu_dispatch;
+    pthread_create(&hilo_cpu_dispatch, NULL, (void *)atender_kernel_cpu_dispatch, NULL);
+    pthread_detach(hilo_cpu_dispatch);
+
+    // Hilo para manejar mensajes de IO
+    pthread_t hilo_entradasalida;
+    pthread_create(&hilo_entradasalida, NULL, (void *)atender_kernel_IO, NULL);
+    pthread_detach(hilo_entradasalida);
+
+    // Hilo para manejar mensajes de Memoria
+    pthread_t hilo_memoria;
+    pthread_create(&hilo_memoria, NULL, (void *)atender_kernel_memoria, NULL);
+    pthread_detach(hilo_memoria);
+}
+
+void cerrar_servidor()
+{
+    free(server_port);
+    free(cpu_dispatch_port);
+    free(memoria_port);
+    liberar_conexion(fd_server);
+    liberar_conexion(fd_kernel_memoria);
+    liberar_conexion(fd_cpu_dispatch);
+    liberar_conexion(fd_kernel_IO);
+}
