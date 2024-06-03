fccdd57b53454f423ac53736309b4ed217e6535e
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-03 21:09:34 +0000

    Paso protipos de atencion de request a kernel_server.h

--- kernel/include/src/protocolo_kernel.h
+++ kernel/include/src/protocolo_kernel.h
@@ -13,9 +13,4 @@ void send_pcb_cpu(t_PCB*);
 // void requests_entradasalida();
 int recv_example_msg_entradasalida();
 
-void atender_kernel_memoria();
-void atender_kernel_IO();
-void atender_kernel_cpu_dispatch();
-
-
 #endif
