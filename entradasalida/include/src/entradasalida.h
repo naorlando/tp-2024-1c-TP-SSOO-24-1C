c1953982cd90cfe31a308342eb72e09d7d223e9a
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-10 23:48:52 +0000

    Funcion main acepta ingreso de parametros por consola (nombre interfaz y path de configuracion) para que cree la interfaz a partir de estos parametros

--- entradasalida/include/src/entradasalida.h
+++ entradasalida/include/src/entradasalida.h
@@ -12,37 +12,8 @@
 #include <commons/log.h>
 #include <utils/sockets.h>
 #include <utils/protocolo.h>
-#include<inicializar_entradasalida.h>
+#include <inicializar_entradasalida.h>
 #include <protocolo_entrada.h>
-
-
-/*
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
- VARIABLES GLOBALES: ir agregando al variables_globales.h
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-*/
-int conexion_io_memoria;
-int conexion_io_kernel;
-int fd_entradasalida;
-int fd_memoria;
-int fd_kernel;
-
-t_log *logger_entradasalida;
-t_log *logger_entradasalida_debug;
-t_config *config_entradasalida;
-// t_entradasalida_config* entradasalida_config;
-
-//Datos de la CONFIG:
-char* TIPO_INTERFAZ;
-int TIEMPO_UNIDAD_TRABAJO;
-char* IP_KERNEL;
-char* PUERTO_KERNEL;
-char* IP_MEMORIA;
-char* PUERTO_MEMORIA;
-char* PATH_BASE_DIALFS;
-int BLOCK_SIZE;
-int BLOCK_COUNT;
-
 
 
 
--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -2,11 +2,19 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 3) {
+        fprintf(stderr, "Uso correcto: %s <nombre_interfaz> <path_config>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-    init();
+    char* nombre_interfaz = argv[1];
+    char* path_config = argv[2];
+
+    init(path_config);
 
     // Comento bloque de conexión con memoria (checkpoint 1)
-    fd_memoria = crear_conexion(logger_entradasalida, SERVER_MEMORIA, IP_MEMORIA, PUERTO_MEMORIA);
+    memoria_port = string_itoa(obtener_puerto_memoria(entradasalida_config));
+    fd_memoria = crear_conexion(logger_entradasalida, SERVER_MEMORIA, obtener_ip_memoria(entradasalida_config), memoria_port);
     fd_memoria > 0 ? send_example_memoria() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_MEMORIA);
 
     // Comento bloque de conexión con kernel (checkpoint 1)
@@ -14,8 +22,8 @@ int main(int argc, char *argv[])
     // fd_kernel > 0 ? send_example_kernel() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_KERNEL);
 
     // Reemplazo el código anterior quitando la conexión con Memoria, ya que la Interfaz Genérica no la necesita
-    
-    fd_kernel = crear_conexion(logger_entradasalida, SERVER_KERNEL, IP_KERNEL, PUERTO_KERNEL);
+    kernel_port = string_itoa(obtener_puerto_kernel(entradasalida_config));
+    fd_kernel = crear_conexion(logger_entradasalida, SERVER_KERNEL, obtener_ip_kernel(entradasalida_config), kernel_port);
     fd_kernel > 0 ? log_info(logger_entradasalida, "Conexión exitosa con kernel")
                   : log_error(logger_entradasalida, "Error al conectar con kernel");
 
@@ -38,6 +46,8 @@ int main(int argc, char *argv[])
     // Liberamos Conexiones
     
     // comento la liberación de la conexión con memoria
+    free(memoria_port);
+    free(kernel_port);
     liberar_conexion(fd_memoria);
     liberar_conexion(fd_kernel);
     return EXIT_SUCCESS;
