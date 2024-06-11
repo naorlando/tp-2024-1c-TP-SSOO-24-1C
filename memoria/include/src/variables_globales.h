d410cedbe1a7142b8bbc2978472c3235401cd748
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-11 23:52:13 +0000

    Reordeno las variables globales y agrego una para el puerto de la memoria

--- memoria/include/src/variables_globales.h
+++ memoria/include/src/variables_globales.h
@@ -12,15 +12,22 @@
 #define CLIENTE_ENTRADASALIDA "CLIENTE ENTRADASALIDA"
 #define MAX_LINE_LENGTH 256
 
+// VARIABLES GLOBALES
+extern t_dictionary* tabla_procesos;
 extern t_log* logger_memoria;
 extern t_log* logger_memoria_debug;
+
+//CONFIG
 extern t_config* config_memoria;
 extern t_memoria_config* memoria_config;
-extern t_dictionary* tabla_procesos;
 
+//FD
 extern int fd_server;
 extern int fd_entradasalida;
 extern int fd_cpu;
 extern int fd_kernel;
+
+//PORTS
+extern char* server_port;
 
 #endif /*VARIABLES_GLOBALES_H_*/
