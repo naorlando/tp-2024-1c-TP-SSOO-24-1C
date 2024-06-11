f350b602e0a055ac8bf5fadb2b494e6aa6b4fecf
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-11 10:06:22 +0000

    Reorganizo el codigo y utilizo las funciones de la libreria entradasalida_server

--- entradasalida/include/src/entradasalida.h
+++ entradasalida/include/src/entradasalida.h
@@ -2,18 +2,11 @@
 #define ENTRADASALIDA_H_
 
 #include<variables_globales.h>
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <pthread.h>
-#include <commons/string.h>
-#include <commons/config.h>
-#include <commons/log.h>
-#include <utils/sockets.h>
-#include <utils/protocolo.h>
 #include <inicializar_entradasalida.h>
-#include <protocolo_entrada.h>
+#include <entradasalida_server.h>
 
 
 
--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -12,43 +12,11 @@ int main(int argc, char *argv[])
 
     init(nombre_interfaz, path_config);
 
-    // Comento bloque de conexión con memoria (checkpoint 1)
-    memoria_port = string_itoa(obtener_puerto_memoria(entradasalida_config));
-    fd_memoria = crear_conexion(logger_entradasalida, SERVER_MEMORIA, obtener_ip_memoria(entradasalida_config), memoria_port);
-    fd_memoria > 0 ? send_example_memoria() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_MEMORIA);
+    inicializar_sockets();
 
-    // Comento bloque de conexión con kernel (checkpoint 1)
-    // fd_kernel = crear_conexion(logger_entradasalida, SERVER_KERNEL, IP_KERNEL, PUERTO_KERNEL);
-    // fd_kernel > 0 ? send_example_kernel() : log_error(logger_entradasalida, "Error al intentar enviar mensaje a %s", SERVER_KERNEL);
+    crear_hilos_conexiones();
 
-    // Reemplazo el código anterior quitando la conexión con Memoria, ya que la Interfaz Genérica no la necesita
-    kernel_port = string_itoa(obtener_puerto_kernel(entradasalida_config));
-    fd_kernel = crear_conexion(logger_entradasalida, SERVER_KERNEL, obtener_ip_kernel(entradasalida_config), kernel_port);
-    fd_kernel > 0 ? log_info(logger_entradasalida, "Conexión exitosa con kernel")
-                  : log_error(logger_entradasalida, "Error al conectar con kernel");
+    cerrar_cliente();
 
-    // Atendemos mensaje del Kernel
-
-    pthread_t hilo_kernel;
-    pthread_create(&hilo_kernel, NULL, (void *)requests_kernel, NULL);
-    pthread_detach(hilo_kernel);
-    //pthread_join(hilo_kernel, NULL);
-
-    //Atender los mensajes de la memoria (comentado), no lo necesitamos para la interfaz genérica
-    pthread_t hilo_memoria;
-    pthread_create(&hilo_memoria, NULL, (void *)requests_memoria,NULL);
-    pthread_join(hilo_memoria,NULL);
-
-    // Agrego un hilo para esperar indefinidamente a recibir solicitudes del kernel (no es necesario un hilo para atender memoria)
-    
-    //pthread_join(hilo_kernel, NULL);
-
-    // Liberamos Conexiones
-    
-    // comento la liberación de la conexión con memoria
-    free(memoria_port);
-    free(kernel_port);
-    liberar_conexion(fd_memoria);
-    liberar_conexion(fd_kernel);
     return EXIT_SUCCESS;
 }
