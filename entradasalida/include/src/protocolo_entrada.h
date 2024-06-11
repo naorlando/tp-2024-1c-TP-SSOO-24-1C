bd6696a96ad94eaed250c0b6daed9c57a372798b
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-11 10:05:03 +0000

    Muevo funciones a 'entradasalida_server'

--- entradasalida/include/src/protocolo_entrada.h
+++ entradasalida/include/src/protocolo_entrada.h
@@ -1,17 +1,13 @@
 #ifndef PROTOCOLO_ENTRADASALIDA_H_
 #define PROTOCOLO_ENTRADASALIDA_H_
 
-
-
-#include "variables_globales.h"
 #include "utils/protocolo.h"
+#include "variables_globales.h"
+#include "utils/estructuras.h"
 
-void requests_kernel();
-void requests_memoria();
 int send_example_kernel();
 int recv_example_msg_kernel();
 int send_example_memoria();
-
 
 void atender_instruccion_sleep();
 // void atender_io_kernel();
