435cf2c91dc70358e99fc975262319b3e3972b3d
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-12 14:47:11 +0000

    Agrego funciones para acceder a los campos de t_kernel_config, agrego descripciones a sus funciones y modifico funcion destruir se agregan free faltantes

--- kernel/include/src/kernel_config.h
+++ kernel/include/src/kernel_config.h
@@ -29,8 +29,75 @@ typedef struct
 
 } t_kernel_config;
 
+// Crea una nueva configuración de kernel.
+// Pre: No hay condiciones previas.
+// Post: Se retorna un puntero a t_kernel_config inicializado o NULL si falla la asignación de memoria.
 t_kernel_config *crear_kernel_config();
+
+// Destruye la configuración del kernel liberando la memoria utilizada.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: La memoria asociada a kc es liberada.
 void destruir(t_kernel_config *);
+
+// Carga la configuración del kernel desde un archivo de configuración.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+//      El puntero cf debe ser un puntero válido a t_config inicializado con un archivo de configuración válido.
+// Post: Se retornará true si la carga fue exitosa, false en caso contrario.
 bool cargar_kernel_config(t_kernel_config *, t_config *);
+
+// Obtiene el puerto de escucha del kernel.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna el puerto de escucha.
+uint16_t obtener_puerto_escucha(t_kernel_config*);
+
+// Obtiene la IP de la memoria.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna un puntero a una cadena con la IP de la memoria.
+char* obtener_ip_memoria(t_kernel_config*);
+
+// Obtiene el puerto de la memoria.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna el puerto de la memoria.
+uint16_t obtener_puerto_memoria(t_kernel_config*);
+
+// Obtiene la IP del CPU.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna un puntero a una cadena con la IP del CPU.
+char* obtener_ip_cpu(t_kernel_config*);
+
+// Obtiene el puerto de dispatch del CPU.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna el puerto de dispatch del CPU.
+uint16_t obtener_puerto_cpu_dispatch(t_kernel_config*);
+
+// Obtiene el puerto de interrupt del CPU.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna el puerto de interrupt del CPU.
+uint16_t obtener_puerto_cpu_interrupt(t_kernel_config*);
+
+// Obtiene el algoritmo de planificación del kernel.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna un puntero a una cadena con el algoritmo de planificación.
+char* obtener_algoritmo_planificacion(t_kernel_config*);
+
+// Obtiene el quantum del kernel.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna el valor del quantum.
+uint16_t obtener_quantum(t_kernel_config*);
+
+// Obtiene los recursos del kernel.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna un puntero a un array de cadenas con los recursos.
+char** obtener_recursos(t_kernel_config*);
+
+// Obtiene las instancias de los recursos del kernel.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna un puntero a un array de enteros con las instancias de los recursos.
+uint32_t* obtener_instancias_recursos(t_kernel_config*);
+
+// Obtiene el grado de multiprogramación del kernel.
+// Pre: El puntero kc debe ser un puntero válido a t_kernel_config.
+// Post: Se retorna el grado de multiprogramación.
+uint16_t obtener_grado_multiprogramacion(t_kernel_config*);
 
 #endif
