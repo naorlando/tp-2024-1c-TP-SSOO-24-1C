3065e1da98a8682987be41521826c71e0b67ac8a
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-12 00:08:51 +0000

    Agregue las funciones 'levantar_servidor', 'esperar_clientes', 'void crear_hilos_conexiones', 'esperar_conexiones_IO(void*)', 'cerrar_servidor()', '_cerrar_puertos()' y '_cerrar_conexiones()' para gestionar las funciones que controlan el funcionamiento del modulo

--- memoria/include/src/memoria_server.h
+++ memoria/include/src/memoria_server.h
@@ -6,6 +6,10 @@
 #include "utils/sockets.h"
 #include "utils/buffer.h"
 #include "utils/estructuras.h"
+#include "memoria_config.h"
+#include <commons/log.h>
+#include <commons/string.h>
+#include <pthread.h>
 
 void requests_cpu();
 
@@ -13,5 +17,18 @@ void requests_kernel();
 
 void requests_entradasalida();
 
+void levantar_servidor();
+
+void esperar_clientes();
+
+void crear_hilos_conexiones();
+
+void* esperar_conexiones_IO(void*);
+
+void cerrar_servidor();
+
+void _cerrar_puertos();
+
+void _cerrar_conexiones();
 
 #endif //MEMORIA_SERVER_H
--- memoria/src/memoria_server.c
+++ memoria/src/memoria_server.c
@@ -124,3 +124,105 @@ void requests_entradasalida() {
         }
     }
 }
+
+void levantar_servidor()
+{
+    server_port = string_itoa(obtener_puerto_escucha(memoria_config));
+    fd_server = iniciar_servidor(logger_memoria, SERVERNAME, NULL, server_port);
+
+    if (fd_server != -1)
+    {
+        log_info(logger_memoria, "%s server listo escuchando en puerto %s", SERVERNAME, server_port);
+    }
+    else
+    {
+        log_error(logger_memoria, "Error al iniciar %s server en puerto %s", SERVERNAME, server_port);
+    }
+}
+
+void esperar_clientes()
+{
+    // Espera conexion de la CPU
+    log_info(logger_memoria, "esperando a que se conecte CPU");
+    fd_cpu = esperar_cliente(logger_memoria, CLIENTE_CPU, fd_server);
+
+    // Espera conexion del Kernel
+    log_info(logger_memoria, "esperando a que se conecte KERNEL");
+    fd_kernel = esperar_cliente(logger_memoria, CLIENTE_KERNEL, fd_server);
+}
+
+void crear_hilos_conexiones() 
+{
+    pthread_t hilo_kernel;
+    pthread_t hilo_cpu;
+    pthread_t hilo_aceptar_io;
+    
+    // Hilo para manejar mensajes del Kernel
+    if (pthread_create(&hilo_kernel, NULL, (void *)requests_kernel, NULL) != 0)
+    {
+        log_error(logger_memoria, "Error al crear el hilo para atender al KERNEL. ABORTANDO");
+        exit(EXIT_FAILURE);
+    }
+
+    // Hilo para manejar mensajes de CPU
+    if (pthread_create(&hilo_cpu, NULL, (void *)requests_cpu, NULL) != 0)
+    {
+        log_error(logger_memoria, "Error al crear el hilo para atender al CPU. ABORTANDO");
+        exit(EXIT_FAILURE);
+    }
+
+    // Creo hilo para aceptar conexiones de IO de forma dinámica
+    if (pthread_create(&hilo_aceptar_io, NULL, esperar_conexiones_IO, &fd_server) != 0) {
+        log_error(logger_memoria, "Error al crear el hilo para aceptar conexiones de IO. ABORTANDO");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_detach(hilo_kernel);
+    pthread_detach(hilo_cpu);
+    pthread_join(hilo_aceptar_io,NULL);
+}
+
+void* esperar_conexiones_IO(void* arg) 
+{
+    int server_fd = *(int*)arg;
+    bool control_key = 1;
+
+    while (control_key) {
+        int cliente_io = esperar_cliente(logger_memoria, CLIENTE_ENTRADASALIDA, server_fd);
+        
+        if (cliente_io != -1) {
+            //add_io_client(cliente_io, "IOClient");
+            
+            pthread_t hilo_entradasalida;
+            if (pthread_create(&hilo_entradasalida, NULL, (void*)requests_entradasalida, &cliente_io) != 0) {
+                log_error(logger_memoria, "Error al crear el hilo para atender el cliente de IO. ABORTANDO");
+                exit(EXIT_FAILURE);
+            }
+            pthread_detach(hilo_entradasalida);
+        } else {
+            log_error(logger_memoria, "Error al esperar cliente de IO");
+        }
+    }
+    return NULL;
+}
+
+void cerrar_servidor()
+{
+    _cerrar_puertos();
+    _cerrar_conexiones();
+    log_info(logger_memoria, "SERVER MEMORIA cerrado correctamente.");
+}
+
+void _cerrar_puertos()
+{
+    free(server_port);
+}
+
+void _cerrar_conexiones()
+{
+    liberar_conexion(fd_server);
+    liberar_conexion(fd_cpu);
+    liberar_conexion(fd_kernel);
+    //TODO: Implementar funcion para liberar las conexiones de todas las IOs
+    // que se conectaron a la memoria
+}
