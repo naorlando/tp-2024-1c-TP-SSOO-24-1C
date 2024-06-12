35382d927eaa4d7a2210847481097a46606a7f70
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-12 14:26:52 +0000

    Agrego importaciones faltantes

--- cpu/include/src/cpu_server.h
+++ cpu/include/src/cpu_server.h
@@ -4,9 +4,13 @@
 #include "variables_globales.h"
 #include "protocolo_cpu.h"
 #include "cpu_dispatch.h"
+#include "cpu_interrupt.h"
 #include "utils/sockets.h"
 #include "utils/buffer.h"
 #include "utils/estructuras.h"
+#include "cpu_config.h"
+#include <commons/log.h>
+#include <pthread.h>
 
 void atender_cpu_kernel_dispatch();
 void atender_cpu_kernel_interrupt();
