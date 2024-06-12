db384a95150fa0c35a382fa379b1ea3d643d1914
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-12 13:19:41 +0000

    Utilizo la nueva funcion 'levnatar_servidor'

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -5,23 +5,8 @@ int main(int argc, char *argv[])
 {
 
     init();
-
-    log_info(logger_cpu, "Archivo de configuracion cargado correctamente");
-
-    // ############## Socket ###################
-
-    char *server_port = string_itoa(cpu_config->PUERTO_ESCUCHA_DISPATCH);
-
-    int fd_server = iniciar_servidor(logger_cpu, NULL, NULL, server_port);
-
-    if (fd_server != -1)
-    {
-        log_info(logger_cpu, "%s server listo escuchando en puerto %s", SERVERNAME, server_port);
-    }
-    else
-    {
-        log_error(logger_cpu, "Error al iniciar %s server en puerto %s", SERVERNAME, server_port);
-    }
+    
+    levantar_servidor();
 
     // Conectamos CPU -> Memoria
 
