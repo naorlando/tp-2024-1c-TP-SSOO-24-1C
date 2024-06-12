0689cad715815b10cb34dd394309096299e2f9ea
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-12 14:24:04 +0000

    Utilizo las funciones de cpu_server

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -5,36 +5,16 @@ int main(int argc, char *argv[])
 {
 
     init();
-    
+
     levantar_servidor();
 
-    // Conectamos CPU -> Memoria
+    inicializar_sockets();
 
-    char *puerto_memoria = string_itoa(cpu_config->PUERTO_MEMORIA);
-    fd_memoria = crear_conexion(logger_cpu, SERVER_MEMORIA, cpu_config->IP_MEMORIA, puerto_memoria);
-    fd_memoria > 0 ? send_example_memoria() : log_error(logger_cpu, "Error al intentar enviar mensaje a %s", SERVER_MEMORIA);
+    esperar_clientes();
 
-    // Esperamos al Kernel
-    log_info(logger_cpu, "esperando a que se conecte %s",CLIENTE_KERNEL);
-    fd_kernel_dispatch = esperar_cliente(logger_cpu, CLIENTE_KERNEL, fd_server);
+    crear_hilos_conexiones();
 
-
-    // Atendemos mensaje del Kernel
-    pthread_t hilo_kernel_dispatch;
-    pthread_create(&hilo_kernel_dispatch, NULL, (void *)atender_cpu_kernel_dispatch, NULL);
-    pthread_detach(hilo_kernel_dispatch);
-
-    //Atender los mensajes de la memoria
-
-    pthread_t hilo_memoria;
-    pthread_create(&hilo_memoria,NULL,(void *)atender_cpu_memoria,NULL);
-    pthread_join(hilo_memoria,NULL);
-
-
-    free(server_port);
-    liberar_conexion(fd_server);
-    liberar_conexion(fd_memoria);
-    liberar_conexion(fd_kernel_dispatch);    
+    cerrar_servidor(); 
 
     return EXIT_SUCCESS;
 }
