c80e8b137e38ec129bab02fca27674d845d9f9d6
Jorge Tripodi <jtripodi@frba.utn.edu.ar>
2024-06-14 03:33:28 +0000

    Modifico entradasalida.c y h para conectar con las nuevas funciones "server" de cada tipo de I/O, y elimino el antiguo "server" unificado.

--- entradasalida/include/src/entradasalida.h
+++ entradasalida/include/src/entradasalida.h
@@ -1,13 +1,14 @@
 #ifndef ENTRADASALIDA_H_
 #define ENTRADASALIDA_H_
 
-#include<variables_globales.h>
+#include <variables_globales.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <inicializar_entradasalida.h>
-#include <entradasalida_server.h>
+#include <entradasalida_server_generica.h>
+#include <entradasalida_server_stdin.h>
+#include <entradasalida_server_stdout.h>
+#include <entradasalida_server_dialfs.h>
 
-
-
-#endif /*ENTRADASALIDA_H_*/
+#endif //ENTRADASALIDA_H_
--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -30,6 +30,5 @@ int main(int argc, char *argv[]) {
     }
 
     cerrar_cliente();
-
     return EXIT_SUCCESS;
 }
