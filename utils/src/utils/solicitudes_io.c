11f4e44ada9679ca38436da4b05169ce0ab5a1a9
Jorge Tripodi <jtripodi@frba.utn.edu.ar>
2024-06-14 01:50:01 +0000

    Agrego estructuras y funciones de solicitudes STDIN y STDOUT.  Ordeno el código en solicitudes_io.c y .h

--- utils/src/utils/solicitudes_io.c
+++ utils/src/utils/solicitudes_io.c
@@ -1,7 +1,10 @@
 #include "solicitudes_io.h"
 #include "buffer.h"
 
-// Serialización
+//===============================================
+//        FUNCIONES DE SERIALIZACION
+//===============================================
+
 void serializar_solicitud_io_generica(t_buffer* buffer, t_solicitud_io_generica* solicitud) {
     serializar_pcb(buffer, solicitud->pcb);
     buffer_add_string(buffer, solicitud->nombre_interfaz);
@@ -13,7 +16,32 @@ void serializar_io_generica(t_buffer* buffer, t_io_generica* io_generica) {
     buffer_add_uint32(buffer, io_generica->tiempo_sleep);
 }
 
-// Deserialización
+void serializar_solicitud_io_stdin(t_buffer* buffer, t_solicitud_io_stdin* solicitud) {
+    serializar_pcb(buffer, solicitud->pcb);
+    buffer_add_string(buffer, solicitud->nombre_interfaz);
+    serializar_io_stdin(buffer, solicitud->stdin);
+}
+
+void serializar_io_stdin(t_buffer* buffer, t_io_stdin* io_stdin) {
+    buffer_add_uint32(buffer, io_stdin->direccion_fisica);
+    buffer_add_uint32(buffer, io_stdin->tamanio);
+}
+
+void serializar_solicitud_io_stdout(t_buffer* buffer, t_solicitud_io_stdout* solicitud) {
+    serializar_pcb(buffer, solicitud->pcb);
+    buffer_add_string(buffer, solicitud->nombre_interfaz);
+    serializar_io_stdout(buffer, solicitud->stdout);
+}
+
+void serializar_io_stdout(t_buffer* buffer, t_io_stdout* io_stdout) {
+    buffer_add_uint32(buffer, io_stdout->direccion_fisica);
+    buffer_add_uint32(buffer, io_stdout->tamanio);
+}
+
+//===============================================
+//        FUNCIONES DE DESERIALIZACIÓN
+//===============================================
+
 t_solicitud_io_generica* deserializar_solicitud_io_generica(t_buffer* buffer) {
     t_PCB* pcb = deserializar_pcb(buffer);
     char* nombre_interfaz = extract_string_buffer(buffer);
@@ -27,7 +55,36 @@ t_io_generica* deserializar_io_generica(t_buffer* buffer) {
     return crear_io_generica(nombre_interfaz, tiempo_sleep);
 }
 
-// Creación
+t_solicitud_io_stdin* deserializar_solicitud_io_stdin(t_buffer* buffer) {
+    t_PCB* pcb = deserializar_pcb(buffer);
+    char* nombre_interfaz = extract_string_buffer(buffer);
+    t_io_stdin* stdin = deserializar_io_stdin(buffer);
+    return crear_solicitud_io_stdin(pcb, nombre_interfaz, stdin);
+}
+
+t_io_stdin* deserializar_io_stdin(t_buffer* buffer) {
+    uint32_t direccion_fisica = buffer_read_uint32(buffer);
+    uint32_t tamanio = buffer_read_uint32(buffer);
+    return crear_io_stdin(direccion_fisica, tamanio);
+}
+
+t_solicitud_io_stdout* deserializar_solicitud_io_stdout(t_buffer* buffer) {
+    t_PCB* pcb = deserializar_pcb(buffer);
+    char* nombre_interfaz = extract_string_buffer(buffer);
+    t_io_stdout* stdout = deserializar_io_stdout(buffer);
+    return crear_solicitud_io_stdout(pcb, nombre_interfaz, stdout);
+}
+
+t_io_stdout* deserializar_io_stdout(t_buffer* buffer) {
+    uint32_t direccion_fisica = buffer_read_uint32(buffer);
+    uint32_t tamanio = buffer_read_uint32(buffer);
+    return crear_io_stdout(direccion_fisica, tamanio);
+}
+
+//===============================================
+//             FUNCIONES DE CREACIÓN
+//===============================================
+
 t_solicitud_io_generica* crear_solicitud_io_generica(t_PCB* pcb, char* nombre_interfaz, t_io_generica* generica) {
     t_solicitud_io_generica* solicitud = malloc(sizeof(t_solicitud_io_generica));
     solicitud->pcb = pcb;
@@ -43,7 +100,40 @@ t_io_generica* crear_io_generica(char* nombre_interfaz, uint32_t tiempo_sleep) {
     return io_generica;
 }
 
-// Destrucción
+t_solicitud_io_stdin* crear_solicitud_io_stdin(t_PCB* pcb, char* nombre_interfaz, t_io_stdin* stdin) {
+    t_solicitud_io_stdin* solicitud = malloc(sizeof(t_solicitud_io_stdin));
+    solicitud->pcb = pcb;
+    solicitud->nombre_interfaz = strdup(nombre_interfaz);
+    solicitud->stdin = stdin;
+    return solicitud;
+}
+
+t_io_stdin* crear_io_stdin(uint32_t direccion_fisica, uint32_t tamanio) {
+    t_io_stdin* io_stdin = malloc(sizeof(t_io_stdin));
+    io_stdin->direccion_fisica = direccion_fisica;
+    io_stdin->tamanio = tamanio;
+    return io_stdin;
+}
+
+t_solicitud_io_stdout* crear_solicitud_io_stdout(t_PCB* pcb, char* nombre_interfaz, t_io_stdout* stdout) {
+    t_solicitud_io_stdout* solicitud = malloc(sizeof(t_solicitud_io_stdout));
+    solicitud->pcb = pcb;
+    solicitud->nombre_interfaz = strdup(nombre_interfaz);
+    solicitud->stdout = stdout;
+    return solicitud;
+}
+
+t_io_stdout* crear_io_stdout(uint32_t direccion_fisica, uint32_t tamanio) {
+    t_io_stdout* io_stdout = malloc(sizeof(t_io_stdout));
+    io_stdout->direccion_fisica = direccion_fisica;
+    io_stdout->tamanio = tamanio;
+    return io_stdout;
+}
+
+//===============================================
+//        FUNCIONES DE DESTRUCCIÓN
+//===============================================
+
 void destruir_solicitud_io_generica(t_solicitud_io_generica* solicitud) {
     free(solicitud->nombre_interfaz);
     destruir_io_generica(solicitud->generica);
@@ -53,4 +143,24 @@ void destruir_solicitud_io_generica(t_solicitud_io_generica* solicitud) {
 void destruir_io_generica(t_io_generica* io_generica) {
     free(io_generica->nombre_interfaz);
     free(io_generica);
+}
+
+void destruir_solicitud_io_stdin(t_solicitud_io_stdin* solicitud) {
+    free(solicitud->nombre_interfaz);
+    destruir_io_stdin(solicitud->stdin);
+    free(solicitud);
+}
+
+void destruir_io_stdin(t_io_stdin* io_stdin) {
+    free(io_stdin);
+}
+
+void destruir_solicitud_io_stdout(t_solicitud_io_stdout* solicitud) {
+    free(solicitud->nombre_interfaz);
+    destruir_io_stdout(solicitud->stdout);
+    free(solicitud);
+}
+
+void destruir_io_stdout(t_io_stdout* io_stdout) {
+    free(io_stdout);
 }
--- utils/src/utils/solicitudes_io.h
+++ utils/src/utils/solicitudes_io.h
@@ -1,10 +1,9 @@
-// solicitudes_io.h
-
 #ifndef SOLICITUDES_IO_H
 #define SOLICITUDES_IO_H
 
 #include "estructuras.h"
 
+// INTERFAZ GENERICA
 typedef struct {
     t_PCB* pcb;
     char* nombre_interfaz;
@@ -16,16 +15,67 @@ typedef struct {
     uint32_t tiempo_sleep;
 } t_io_generica;
 
-// Funciones para serializar y deserializar las estructuras
+// INTERFAZ STDIN
+typedef struct {
+    t_PCB* pcb;
+    char* nombre_interfaz;
+    t_io_stdin* stdin;
+} t_solicitud_io_stdin;
+
+typedef struct {
+    uint32_t direccion_fisica;
+    uint32_t tamanio;
+} t_io_stdin;
+
+// INTERFAZ STDOUT
+typedef struct {
+    t_PCB* pcb;
+    char* nombre_interfaz;
+    t_io_stdout* stdout;
+} t_solicitud_io_stdout;
+
+typedef struct {
+    uint32_t direccion_fisica;
+    uint32_t tamanio;
+} t_io_stdout;
+
+
+//===============================================
+// FUNCIONES DE SERIALIZACION Y DESERIALIZACIÓN
+//===============================================
+
 void serializar_solicitud_io_generica(t_buffer* buffer, t_solicitud_io_generica* solicitud);
 void serializar_io_generica(t_buffer* buffer, t_io_generica* io_generica);
 t_solicitud_io_generica* deserializar_solicitud_io_generica(t_buffer* buffer);
 t_io_generica* deserializar_io_generica(t_buffer* buffer);
 
-// Funciones para crear y destruir las estructuras
+void serializar_solicitud_io_stdin(t_buffer* buffer, t_solicitud_io_stdin* solicitud);
+void serializar_io_stdin(t_buffer* buffer, t_io_stdin* io_stdin);
+t_solicitud_io_stdin* deserializar_solicitud_io_stdin(t_buffer* buffer);
+t_io_stdin* deserializar_io_stdin(t_buffer* buffer);
+
+void serializar_solicitud_io_stdout(t_buffer* buffer, t_solicitud_io_stdout* solicitud);
+void serializar_io_stdout(t_buffer* buffer, t_io_stdout* io_stdout);
+t_solicitud_io_stdout* deserializar_solicitud_io_stdout(t_buffer* buffer);
+t_io_stdout* deserializar_io_stdout(t_buffer* buffer);
+
+//===============================================
+//     FUNCIONES DE CREACIÓN Y DESTRUCCIÓN
+//===============================================
+
 t_solicitud_io_generica* crear_solicitud_io_generica(t_PCB* pcb, char* nombre_interfaz, t_io_generica* generica);
 t_io_generica* crear_io_generica(char* nombre_interfaz, uint32_t tiempo_sleep);
 void destruir_solicitud_io_generica(t_solicitud_io_generica* solicitud);
 void destruir_io_generica(t_io_generica* io_generica);
+
+t_solicitud_io_stdin* crear_solicitud_io_stdin(t_PCB* pcb, char* nombre_interfaz, t_io_stdin* stdin);
+t_io_stdin* crear_io_stdin(uint32_t direccion_fisica, uint32_t tamanio);
+void destruir_solicitud_io_stdin(t_solicitud_io_stdin* solicitud);
+void destruir_io_stdin(t_io_stdin* io_stdin);
+
+t_solicitud_io_stdout* crear_solicitud_io_stdout(t_PCB* pcb, char* nombre_interfaz, t_io_stdout* stdout);
+t_io_stdout* crear_io_stdout(uint32_t direccion_fisica, uint32_t tamanio);
+void destruir_solicitud_io_stdout(t_solicitud_io_stdout* solicitud);
+void destruir_io_stdout(t_io_stdout* io_stdout);
 
 #endif
