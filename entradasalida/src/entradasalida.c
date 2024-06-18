9922074a20a3af0fa99e5a0cfb51ddba721ee875
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-06-18 12:06:44 +0000

    Utilizo funcion 'crear_hilos_conexiones'

--- entradasalida/src/entradasalida.c
+++ entradasalida/src/entradasalida.c
@@ -15,21 +15,23 @@ int main(int argc, char *argv[]) {
     inicializar_sockets();
 
     // Consultar el tipo de interfaz
-    char* tipo_interfaz = obtener_tipo_interfaz(entradasalida_config);
+    //char* tipo_interfaz = obtener_tipo_interfaz(entradasalida_config);
 
-    // Crear hilos según el tipo de interfaz
-    if (strcmp(tipo_interfaz, "GENERICA") == 0) {
-        crear_hilo_interfaz_generica();
-    } else if (strcmp(tipo_interfaz, "STDIN") == 0) {
-        crear_hilo_interfaz_stdin();
-    } else if (strcmp(tipo_interfaz, "STDOUT") == 0) {
-        crear_hilo_interfaz_stdout();
-    } else if (strcmp(tipo_interfaz, "DIALFS") == 0) {
-        crear_hilo_interfaz_dialfs();
-    } else {
-        fprintf(stderr, "Tipo de interfaz desconocido: %s\\n", tipo_interfaz);
-        exit(EXIT_FAILURE);
-    }
+    // // Crear hilos según el tipo de interfaz
+    // if (strcmp(tipo_interfaz, "GENERICA") == 0) {
+    //     crear_hilo_interfaz_generica();
+    // } else if (strcmp(tipo_interfaz, "STDIN") == 0) {
+    //     crear_hilo_interfaz_stdin();
+    // } else if (strcmp(tipo_interfaz, "STDOUT") == 0) {
+    //     crear_hilo_interfaz_stdout();
+    // } else if (strcmp(tipo_interfaz, "DIALFS") == 0) {
+    //     crear_hilo_interfaz_dialfs();
+    // } else {
+    //     fprintf(stderr, "Tipo de interfaz desconocido: %s\\n", tipo_interfaz);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    crear_hilos_conexiones();
 
     cerrar_cliente();
 
