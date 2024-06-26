ca9f6bff8d0458dff9e158082fa6ff7a1dbb127e
Jorge Tripodi <jtripodi@frba.utn.edu.ar>
2024-06-26 01:31:23 +0000

    Modifico implementación que controla las funciones de DialFS, y las declaro también.

--- entradasalida/include/src/protocolo_entradasalida.h
+++ entradasalida/include/src/protocolo_entradasalida.h
@@ -4,16 +4,25 @@
 #include "utils/protocolo.h"
 #include "variables_globales.h"
 #include "utils/estructuras.h"
+#include "dialfs.h"
 
+// FUNCIONES de atención de instrucciones
 void atender_instruccion_generica(int fd);
 void atender_instruccion_stdin(int fd);
 void atender_instruccion_stdout(int fd);
+void atender_instruccion_dialfs(int fd);
 
+// FUNCION auxiliar de interfaz STDIN
 void escribir_memoria(uint32_t direccion_fisica, char* valor, uint32_t tamanio);
+
+// FUNCION auxiliar de interfaz STDOUT
 char* leer_memoria(uint32_t direccion_fisica, uint32_t tamanio);
 
-void enviar_confirmacion_io(int fd);
+// FUNCION auxiliar de interfaz DIALFS
+void enviar_datos_leidos(int fd, void* buffer, uint32_t tamanio);
 
+// FUNCIONES comunes
+void enviar_confirmacion_io(int fd);
 void send_IO_interface_kernel();
 
 #endif
