f4e97cffc068e4da98a2c8bc2ab88702f5813b7d
Jorge Tripodi <jtripodi@frba.utn.edu.ar>
2024-07-12 03:30:05 +0000

    Ajusto definiciones kernel_server.h a las nuevas funciones

--- kernel/include/src/kernel_server.h
+++ kernel/include/src/kernel_server.h
@@ -7,6 +7,7 @@
 #include "utils/sockets.h"
 //#include "utils/estructuras.h"
 //#include "utils/buffer.h"
+#include <commons/collections/dictionary.h>
 #include <commons/log.h>
 #include "kernel_config.h"
 #include <pthread.h>
@@ -14,17 +15,32 @@
 #include "manager_dispatch.h"
 #include "manager_ios.h"
 
-void atender_kernel_memoria();
-void atender_kernel_IO(void*);
-void atender_kernel_cpu_dispatch();
 void levantar_servidor();
 void inicializar_sockets();
 void crear_hilos_conexiones();
 void cerrar_servidor();
-void* esperar_conexiones_IO(void*);
+void* esperar_conexiones_IO(void* arg);
+//void cancelar_hilo_quantum(uint32_t);
+
+// Funciones de manejo de conexiones memoria
+void atender_kernel_memoria();
+
+// Funciones de manejo de conexiones CPU
+void atender_kernel_cpu_dispatch();
+
+// Funciones de manejo de conexiones I/O
+t_IO_connection* recibir_io_connection(int cliente_io);
+void agregar_IO_connection(t_IO_connection* io_connection);
+t_IO_connection* get_IO_connection(char* nombre_interfaz);
+void liberar_IO_connection(t_IO_connection* io_connection);
+
+// Funciones auxiliares
+int obtener_file_descriptor(t_IO_connection* conexion);
+char* obtener_nombre_conexion(t_IO_connection* conexion);
+tipo_interfaz_t obtener_tipo_conexion(t_IO_connection* conexion);
+
+// Funciones de cierre
 void _cerrar_conexiones();
 void _cerrar_puertos();
-char* recibir_io_connection(int);
-//void cancelar_hilo_quantum(uint32_t);
 
 #endif //KERNEL_SERVER
