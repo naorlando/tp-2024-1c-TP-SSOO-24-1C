25f5eccb65319965924eb0fe04963214b0c74afd
Jorge Tripodi <jtripodi@frba.utn.edu.ar>
2024-07-15 22:10:09 +0000

    Reestructuración protocolo_entradasalida.c y .h

--- entradasalida/include/src/protocolo_entradasalida.h
+++ entradasalida/include/src/protocolo_entradasalida.h
@@ -1,26 +1,78 @@
-#ifndef PROTOCOLO_ENTRADA_H_
-#define PROTOCOLO_ENTRADA_H_
+#ifndef PROTOCOLO_ENTRADASALIDA_H_
+#define PROTOCOLO_ENTRADASALIDA_H_
 
-#include <readline/readline.h>
 #include "variables_globales.h"
 #include "utils/protocolo.h"
 #include "utils/estructuras.h"
 #include "comportamientos_ios.h"
-#include "utils/solicitudes_io.h" // ??
+#include "utils/solicitudes_io.h" 
 #include "dialfs.h"
 
-// FUNCIONES de atención de instrucciones
-void atender_solicitud_generica(int);
-void atender_solicitud_stdin(int);
-void atender_solicitud_stdout(int);
-void atender_solicitud_dialfs(int fd);
+//======================================================
+//        FUNCIONES DE ENTRADA/SALIDA GENERICA
+//======================================================
 
-void send_confirmacion_io(int, t_msg_header, t_response*);
+/**
+ * Atiende una solicitud genérica de entrada/salida.
+ * Esta función espera un tiempo determinado según la solicitud.
+ *
+ * @param fd Descriptor de archivo para la comunicación.
+ */
+void atender_solicitud_generica(int fd);
 
+//======================================================
+//        FUNCIONES DE ENTRADA/SALIDA STDIN
+//======================================================
 
+/**
+ * Atiende una solicitud de entrada/salida STDIN.
+ * Lee un texto desde la consola y lo guarda en memoria.
+ *
+ * @param fd Descriptor de archivo para la comunicación.
+ */
+void atender_solicitud_stdin(int fd);
 
+//======================================================
+//        FUNCIONES DE ENTRADA/SALIDA STDOUT
+//======================================================
 
-// FUNCIONES comunes
+/**
+ * Atiende una solicitud de entrada/salida STDOUT.
+ * Lee un texto desde memoria y lo muestra por consola.
+ *
+ * @param fd Descriptor de archivo para la comunicación.
+ */
+void atender_solicitud_stdout(int fd);
+
+//======================================================
+//        FUNCIONES DE ENTRADA/SALIDA DIALFS
+//======================================================
+
+/**
+ * Atiende una solicitud de entrada/salida DialFS.
+ * Maneja operaciones de creación, eliminación, truncado, lectura y escritura de archivos.
+ *
+ * @param fd Descriptor de archivo para la comunicación.
+ * @param dialfs Puntero a la estructura t_dialfs que representa el sistema de archivos.
+ */
+void atender_solicitud_dialfs(int fd, t_dialfs* dialfs);
+
+//======================================================
+//               FUNCIONES COMUNES
+//======================================================
+
+/**
+ * Envía una confirmación de la operación de entrada/salida.
+ *
+ * @param fd Descriptor de archivo para la comunicación.
+ * @param header Encabezado del mensaje de confirmación.
+ * @param response Respuesta a enviar.
+ */
+void send_confirmacion_io(int fd, t_msg_header header, t_response* response);
+
+/**
+ * Envía la información de la interfaz de E/S al kernel.
+ */
 void send_IO_interface_kernel();
 
-#endif
+#endif // PROTOCOLO_ENTRADASALIDA_H_
--- entradasalida/src/protocolo_entradasalida.c
+++ entradasalida/src/protocolo_entradasalida.c
@@ -1,12 +1,9 @@
 #include "protocolo_entradasalida.h"
 
-
 //======================================================
 //        FUNCIONES DE ENTRADA/SALIDA GENERICA
 //======================================================
 
-// Función que atiende una solicitud genérica de entrada/salida, 
-// la cual consiste en esperar un tiempo determinado
 void atender_solicitud_generica(int fd) {
     t_io_generica *io_generica = recv_io_generica(fd);
 
@@ -27,11 +24,9 @@ void atender_solicitud_generica(int fd) {
 }
 
 //======================================================
-//          FUNCIONES DE ENTRADA/SALIDA STDIN
+//        FUNCIONES DE ENTRADA/SALIDA STDIN
 //======================================================
 
-// Función que atiende una solicitud de entrada/salida STDIN,
-// la cual consiste en leer un texto desde la consola y guardarlo en memoria
 void atender_solicitud_stdin(int fd) {
     t_io_stdin *io_stdin = recv_io_stdin(fd);
 
@@ -45,7 +40,6 @@ void atender_solicitud_stdin(int fd) {
 
         log_info(logger_entradasalida, "Operacion READ finalizada");
 
-        //TODO: modificar la funcion para que no este harcodeado el 'true'
         t_response* response = create_response(true, obtener_pid_stdin(io_stdin));
         send_confirmacion_io(fd, MSG_IO_KERNEL_STDIN, response);
 
@@ -56,11 +50,9 @@ void atender_solicitud_stdin(int fd) {
 }
 
 //======================================================
-//          FUNCIONES DE ENTRADA/SALIDA STDOUT
+//        FUNCIONES DE ENTRADA/SALIDA STDOUT
 //======================================================
 
-// Función que atiende una solicitud de entrada/salida STDOUT,
-// la cual consiste en leer un texto desde memoria y mostrarlo por consola
 void atender_solicitud_stdout(int fd) {
     t_io_stdout* io_stdout = recv_io_stdout(fd);
 
@@ -72,7 +64,6 @@ void atender_solicitud_stdout(int fd) {
 
         free(valor);
 
-        //TODO: modificar la funcion para que no este harcodeado el 'true'
         t_response* response = create_response(true, obtener_pid_stdout(io_stdout));
         send_confirmacion_io(fd, MSG_IO_KERNEL_STDOUT, response);
 
@@ -85,34 +76,34 @@ void atender_solicitud_stdout(int fd) {
 }
 
 //======================================================
-//          FUNCIONES DE ENTRADA/SALIDA DIALFS
+//        FUNCIONES DE ENTRADA/SALIDA DIALFS
 //======================================================
 
-void atender_solicitud_dialfs(int fd) {
+void atender_solicitud_dialfs(int fd, t_dialfs* dialfs) {
     t_io_dialfs* io_dialfs = deserializar_io_dialfs(recibir_buffer(&fd, sizeof(int)));
     if (io_dialfs != NULL) {
         bool operacion_exitosa = false;
         
         switch(io_dialfs->operacion) {
             case IO_FS_CREATE:
-                operacion_exitosa = crear_archivo(io_dialfs, io_dialfs->nombre_archivo);
+                operacion_exitosa = crear_archivo(dialfs, io_dialfs->nombre_archivo);
                 log_info(logger_entradasalida, "PID: %d - Crear Archivo: %s", io_dialfs->pid, io_dialfs->nombre_archivo);
                 break;
             case IO_FS_DELETE:
-                operacion_exitosa = eliminar_archivo(io_dialfs, io_dialfs->nombre_archivo);
+                operacion_exitosa = eliminar_archivo(dialfs, io_dialfs->nombre_archivo);
                 log_info(logger_entradasalida, "PID: %d - Eliminar Archivo: %s", io_dialfs->pid, io_dialfs->nombre_archivo);
                 break;
             case IO_FS_TRUNCATE:
-                operacion_exitosa = truncar_archivo(io_dialfs, io_dialfs->nombre_archivo, io_dialfs->tamanio);
+                operacion_exitosa = truncar_archivo(dialfs, io_dialfs->nombre_archivo, io_dialfs->tamanio);
                 log_info(logger_entradasalida, "PID: %d - Truncar Archivo: %s - Tamaño: %d", io_dialfs->pid, io_dialfs->nombre_archivo, io_dialfs->tamanio);
                 break;
             case IO_FS_WRITE:
-                operacion_exitosa = escribir_archivo(io_dialfs, io_dialfs->nombre_archivo, io_dialfs->datos, io_dialfs->tamanio, io_dialfs->offset);
+                operacion_exitosa = escribir_archivo(dialfs, io_dialfs->nombre_archivo, io_dialfs->datos, io_dialfs->tamanio, io_dialfs->offset);
                 log_info(logger_entradasalida, "PID: %d - Escribir Archivo: %s - Tamaño a Escribir: %d - Puntero Archivo: %d", io_dialfs->pid, io_dialfs->nombre_archivo, io_dialfs->tamanio, io_dialfs->offset);
                 break;
             case IO_FS_READ:
                 void* buffer = malloc(io_dialfs->tamanio);
-                operacion_exitosa = leer_archivo(io_dialfs, io_dialfs->nombre_archivo, buffer, io_dialfs->tamanio, io_dialfs->offset);
+                operacion_exitosa = leer_archivo(dialfs, io_dialfs->nombre_archivo, buffer, io_dialfs->tamanio, io_dialfs->offset);
                 if (operacion_exitosa) {
                     enviar_datos_leidos(fd, buffer, io_dialfs->tamanio);
                 }
@@ -124,23 +115,16 @@ void atender_solicitud_dialfs(int fd) {
                 break;
         }
         
-        if (io_dialfs->operacion == IO_FS_READ && operacion_exitosa) {
-            // Ya se enviaron los datos leídos, no es necesario enviar confirmación adicional
-        } else {
-            enviar_confirmacion_io(fd, operacion_exitosa);
+        if (io_dialfs->operacion != IO_FS_READ || !operacion_exitosa) {
+            t_response* response = create_response(operacion_exitosa, io_dialfs->pid);
+            send_confirmacion_io(fd, MSG_IO_KERNEL_DIALFS, response);
         }
         
         destruir_io_dialfs(io_dialfs);
     } else {
         log_error(logger_entradasalida, "Error al recibir IO DialFS");
-        enviar_confirmacion_io(fd, false);
-    }
-    
-    // Comprobamos si es necesario realizar una compactación
-    if (es_necesario_compactar(dialfs)) {
-        log_info(logger_entradasalida, "Inicio Compactación.");
-        compactar_fs(dialfs);
-        log_info(logger_entradasalida, "Fin Compactación.");
+        t_response* response = create_response(false, 0);
+        send_confirmacion_io(fd, MSG_IO_KERNEL_DIALFS, response);
     }
 }
 
@@ -148,7 +132,6 @@ void atender_solicitud_dialfs(int fd) {
 //               FUNCIONES COMUNES
 //======================================================
 
-// Función auxiliar que envía una confirmación de la operación de entrada/salida
 void send_confirmacion_io(int fd, t_msg_header header, t_response* response) {
     send_response(fd, header, response);
 }
