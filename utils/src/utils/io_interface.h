2a76eacca3a27acc575a02e66f7933deab6b0d3e
FrancoGroselle <fgroselle@frba.utn.edu.ar>
2024-07-21 18:36:10 +0000

    Elimino funciones y las paso a la nueva libreria 'gestion_conexiones_io'

--- utils/src/utils/io_interface.h
+++ utils/src/utils/io_interface.h
@@ -8,10 +8,7 @@
 #include <commons/collections/queue.h>
 #include <semaphore.h>
 #include <pthread.h>
-#include <commons/collections/dictionary.h>
-#include <commons/log.h>
-#include "protocolo.h"
-#include "sockets.h"
+
 
 // Definición del enum tipo_interfaz_t
 typedef enum {
@@ -146,30 +143,5 @@ bool agregar_proceso_bloqueado(t_IO_connection* conexion, void* proceso);
 // Pre: El puntero a t_IO_connection debe ser válido y no NULL.
 // Post: Retorna true si la cola de procesos bloqueados está vacía, false en caso contrario.
 bool tiene_procesos_bloqueados(t_IO_connection* cliente_io);
-
-//========================================================
-// FUNCIONES AVANZADAS DE T_IO_CONNECTION CON T_DICTIONARY
-//========================================================
-
-// Obtiene una conexión de entrada/salida a partir de su nombre.
-// Pre: El puntero a la cadena de nombre_interfaz y los punteros a t_dictionary y pthread_mutex_t deben ser válidos y no NULL.
-// Post: Retorna un puntero a t_IO_connection si la conexión existe, o NULL si no se encuentra.
-t_IO_connection* get_IO_connection(char* nombre_interfaz, t_dictionary* io_connections, pthread_mutex_t* mutex_dictionary);
-
-// Agrega una nueva conexión de entrada/salida al diccionario de conexiones.
-// Pre: Los punteros a t_IO_connection, t_dictionary y pthread_mutex_t deben ser válidos y no NULL.
-// Post: Agrega la conexión al diccionario si no existe previamente.
-void agregar_IO_connection(t_IO_connection* io_connection, t_dictionary* io_connections, pthread_mutex_t* mutex_dictionary);
-
-// Crea una nueva conexión de entrada/salida para un cliente conectado.
-// Pre: El file descriptor del cliente (cliente_io) debe ser válido. El puntero a t_log debe ser válido y no NULL.
-// Post: Retorna un puntero a t_IO_connection si la conexión fue creada exitosamente, o NULL si hubo un error.
-t_IO_connection* nuevo_IO_cliente_conectado(int cliente_io, t_log* logger);
-
-// Recibe y valida una nueva conexión de entrada/salida para un cliente.
-// Pre: El file descriptor del cliente (cliente_io), el puntero a t_log y el header_valido deben ser válidos.
-// Post: Retorna un puntero a t_IO_connection si la conexión fue recibida y validada exitosamente, o NULL si hubo un error.
-t_IO_connection* recibir_io_connection(int cliente_io, t_log* logger,  t_msg_header header_valido);
-
 
 #endif // IO_INTERFACE_H
